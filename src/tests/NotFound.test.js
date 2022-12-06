e0fe1fd86a9414c20bfd2999f79e3737f55d643f
Henrique Vilela <hv_santos@outlook.com>
2022-12-05 22:15:30 -0300

    A 4 foi

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('4. Teste o componente <NotFound.js />', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundTitle = screen.getByText(/Page requested not found/i);
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+  test('Teste se a página mostra uma imagem', () => {
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    renderWithRouter(<NotFound />);
+    const notFoundImg = screen.getByRole('img');
+    expect(notFoundImg.src).toBe(url);
+  });
+});
