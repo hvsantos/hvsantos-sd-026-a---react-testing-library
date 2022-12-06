d7332bb132ad46bf610f0a084ee89991bdc402bd
Henrique Vilela <hv_santos@outlook.com>
2022-12-05 22:10:24 -0300

    Foi a 3 TB

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemon } from '../pages';
+
+describe('3. Teste o componente <FavoritePokemon.js />', () => {
+  test('', () => {
+    renderWithRouter(<FavoritePokemon />);
+    const textInScreen = screen.getByText(/No favorite pokémon found/i);
+    expect(textInScreen).toHaveTextContent(/No favorite pokémon found/i);
+  });
+});
