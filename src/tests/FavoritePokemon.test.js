2846bff0942652150f09e2313c0a55eb4fa66735
Henrique Vilela <hv_santos@outlook.com>
2022-12-05 22:12:09 -0300

    fix test msg

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -4,7 +4,7 @@ import renderWithRouter from '../renderWithRouter';
 import { FavoritePokemon } from '../pages';
 
 describe('3. Teste o componente <FavoritePokemon.js />', () => {
-  test('', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokémon found', () => {
     renderWithRouter(<FavoritePokemon />);
     const textInScreen = screen.getByText(/No favorite pokémon found/i);
     expect(textInScreen).toHaveTextContent(/No favorite pokémon found/i);
